8e82335c22407bccd4f37f9edfa8e712540ba353
Steinar Bang <steb@steria.no>
2015-01-21 22:52:03 +0100

    Created Confluence macro class.

---
+++ src/sdkintro/src/main/java/no/steria/jira/confluence/plugins/sdkintro/macros/HelloJiraIssueMacro.java
@@ -0,0 +1,30 @@
+package no.steria.jira.confluence.plugins.sdkintro.macros;
+
+import java.util.Map;
+
+import com.atlassian.confluence.content.render.xhtml.ConversionContext;
+import com.atlassian.confluence.macro.Macro;
+import com.atlassian.confluence.macro.MacroExecutionException;
+
+public class HelloJiraIssueMacro implements Macro {
+
+	@Override
+	public String execute(Map<String, String> arg0, String arg1,
+			ConversionContext arg2) throws MacroExecutionException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public BodyType getBodyType() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public OutputType getOutputType() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
