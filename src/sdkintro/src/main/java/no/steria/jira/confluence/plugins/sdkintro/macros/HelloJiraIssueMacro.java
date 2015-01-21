98a248d76575a90b09bda4f0562a070d17a942a2
Steinar Bang <steb@steria.no>
2015-01-21 23:09:58 +0100

    Report missing issue key parameter with exception.

--- src/sdkintro/src/main/java/no/steria/jira/confluence/plugins/sdkintro/macros/HelloJiraIssueMacro.java
+++ src/sdkintro/src/main/java/no/steria/jira/confluence/plugins/sdkintro/macros/HelloJiraIssueMacro.java
@@ -11,7 +11,10 @@ public class HelloJiraIssueMacro implements Macro {
 	@Override
 	public String execute(Map<String, String> parameters, String body, ConversionContext context) throws MacroExecutionException {
 	        String jiraIssueKey = parameters.get("jiraIssueKey");
-	        return "<b>HelloJiraIssue macro for issue: " + jiraIssueKey + "</b>";
+	        if (null == jiraIssueKey) {
+	                throw new MacroExecutionException("A jiraIssueKey referencing a valid Jira issue is required");
+	        }
+	        return "<b>HelloJiraIssue macro for:" + jiraIssueKey +"</b>";
 	}
 
 	@Override
